ede208c6a33b160fc25fc3976bdaf73f18e76e57
Felix Dangers <fdangers@proton.me>
2025-03-23 16:42:05 +0100

    Remove build dir from repo and ignore it

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/felix/Projects/Coursera/linux_systemprogrammierung_einfuehrung_buildroot/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
